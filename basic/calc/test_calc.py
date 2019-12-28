e1ae31886a99d11796dca588fd8d9c6a88cd9eb5
AsakoO <okazakiasako@gmail.com>
2019-12-28 17:12:19 +0900

    update calc_test.py
    テストを足し算と引き算で分ける

--- basic/calc/test_calc.py
+++ basic/calc/test_calc.py
@@ -1,9 +1,11 @@
 import pytest
 import calc
 
-
 @pytest.mark.parametrize(
-    ["test_input", "expected"], [([10, 5], (15, 5)), ([3, 10], (13, -7)),],
+    ["test_input", "expectedadd","expectedsub"] ,[([10, 5],15,5),([3, 10],13,-7)]
 )
-def test_calc(test_input, expected):
-    assert calc.calc(*test_input) == expected
+
+#この関数にはparametrizeで定義した数を全て渡す
+def test_calcadd(test_input, expectedadd,expectedsub):
+    assert calc.calcadd(*test_input) == expectedadd
+    assert calc.calcsub(*test_input) == expectedsub
