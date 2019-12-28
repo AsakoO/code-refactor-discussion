1004207ec26e61e9b53797c16ba70ea768db9b1c
AsakoO <okazakiasako@gmail.com>
2019-12-28 17:11:05 +0900

    update calc.py
    addとsubの関数分け

--- basic/calc/calc.py
+++ basic/calc/calc.py
@@ -1,15 +1,21 @@
 """ 2つの値の足し算と引き算の結果を表示する. """
 
 
-def calc(num1, num2):
+def calcadd(num1, num2):
     result_add = num1 + num2
+    return result_add
+
+def calcsub(num1, num2):
     result_sub = num1 - num2
+    return result_sub
 
-    return result_add, result_sub
-
-
+#ここはimportされた場合実行されない
 if __name__ == "__main__":
-    add, sub = calc(10, 5)
-
-    print(f"{add=}")
-    print(f"{sub=}")
+    num1,num2=10,5
+    add = calcadd(num1, num2)
+    sub = calcsub(num1, num2)
+    
+    print(f"{add}")
+    print(f"{sub}")
+    #print(f"{add=}") #python3.8じゃないと動かない
+    #print(f"{sub=}") #python3.8じゃないと動かない
